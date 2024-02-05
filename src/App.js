dfb1bbc6be0c79862c6172837575b39b6c8afc3d
letrieuthien <ltthien001@gmail.com>
2024-02-05 14:32:23 +0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,138 +1,20 @@
-import "./App.css";
-import { useState, useEffect } from "react";
-import * as BooksApi from "./BooksAPI"
-import {shelfEnum} from "./core/enum";
-import BookItem from "./BookItem";
+import {BrowserRouter as Router,Routes, Route} from "react-router-dom";
+import {useState} from "react";
+import BookList from "./BookList";
+import SearchComponent from "./Search";
 
 function App() {
-  const [showSearchPage, setShowSearchpage] = useState(false);
-  const [bookList,setBookList] = useState([])
-  const [bookResultList,setBookResultList] = useState([])
-  const [keyWord,setKeyWord] = useState('')
-
-  const getBookList = async () => {
-    const res = await BooksApi.getAll();
-    if(res) {
-      setBookList(res)
-    }
-  };
-
-  useEffect(() => {
-    if(showSearchPage) {
-      const delayDebounceFn = setTimeout(() => {
-        try {
-          BooksApi.search(keyWord, 100).then(res => {
-            if(res) {
-              BooksApi.search(keyWord, 100).then(res => {
-                setBookResultList(res);
-              });
-            } else {
-              setBookResultList([])
-            }
-          })
-        } catch (e) {
-          setBookResultList([])
-        }
-      }, 1000)
-      return () => clearTimeout(delayDebounceFn)
-    } else {
-      getBookList().then(r => {});
-    }
-
-  }, [keyWord])
-
-  const handleCallBackChangeShelf = (data) => {
-    if(!data) return;
-    BooksApi.update(data.bookID, data.shelf).then(res => {
-      if(res) {
-        getBookList().then();
-      }
-    })
-
-  };
-
-  const handleChangeKeyWord = (event) => {
-    setKeyWord(event.target.value);
-  };
-
+    const [keyWord,setKeyWord] = useState([''])
+    const handleSetKeyWord = (data) => {
+        setKeyWord(data)
+    };
   return (
-    <div className="app">
-      {showSearchPage ? (
-        <div className="search-books">
-          <div className="search-books-bar">
-            <a
-              className="close-search"
-              onClick={() => setShowSearchpage(!showSearchPage)}
-            >
-              Close
-            </a>
-            <div className="search-books-input-wrapper">
-              <input
-                type="text"
-                placeholder="Search by title, author, or ISBN"
-                onChange={handleChangeKeyWord}
-                value={keyWord}
-              />
-            </div>
-          </div>
-          <div className="search-books-results">
-            <ol className="books-grid">
-              {
-                bookResultList.length > 0 ? (
-                bookResultList.map((book, index) =>
-                    (<BookItem book={book} key={index} parentCallback={handleCallBackChangeShelf}/>))) : null
-              }
-            </ol>
-          </div>
-        </div>
-      ) : (
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
-          <div className="list-books-content">
-            <div>
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
-                <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {
-                      bookList.filter(book => book.shelf === shelfEnum.READING).map((book, index) =>
-                          (<BookItem book={book} key={index} parentCallback={handleCallBackChangeShelf}/>))
-                    }
-                  </ol>
-                </div>
-              </div>
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">Want to Read</h2>
-                <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {
-                      bookList.filter(book => book.shelf === shelfEnum.WANT_TO_READ).map((book, index) =>
-                          (<BookItem book={book} key={index} parentCallback={handleCallBackChangeShelf}/>))
-                    }
-                  </ol>
-                </div>
-              </div>
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">Read</h2>
-                <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {
-                      bookList.filter(book => book.shelf === shelfEnum.READ).map((book, index) =>
-                          (<BookItem book={book} key={index} parentCallback={handleCallBackChangeShelf}/>))
-                    }
-                  </ol>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="open-search">
-            <a onClick={() => setShowSearchpage(!showSearchPage)}>Add a book</a>
-          </div>
-        </div>
-      )}
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<BookList />}/>
+          <Route path="/search" element={<SearchComponent keyWord={keyWord} parentCallback={handleSetKeyWord} />}/>
+        </Routes>
+      </Router>
   );
 }
 
--- src/BookItem.js
+++ src/BookItem.js
@@ -1,33 +1,18 @@
 import {shelfEnum} from "./core/enum";
+import * as BooksApi from "./BooksAPI";
 
 export const BookItem = (props) => {
-    function moveShelf(event) {
-        const bookData = event.target.value.split(',');
+    const book = props.book;
+    function moveShelf(e) {
+        const bookData = e.split(',');
         if(bookData.length < 2) return;
-        const payload = {
-            bookID: bookData[0],
-            shelf: bookData[1]
-        }
-        props.parentCallback(payload);
+        BooksApi.update(bookData[0], bookData[1]).then(res => {
+            if(!res) return;
+            props.parentCallback();
+        })
     }
 
-    const renderOptional = (book) => {
-        const rex = /([A-Z])([A-Z])([a-z])|([a-z])([A-Z])/g;
-        const optionalValue = [
-           {bookID: book.id, shelf: shelfEnum.READING},
-           {bookID: book.id, shelf: shelfEnum.WANT_TO_READ},
-           {bookID: book.id, shelf: shelfEnum.READ},
-           {bookID: book.id, shelf: shelfEnum.NONE}
-        ]
-        const data = [];
-            optionalValue.filter(optional => (optional.shelf !== book.shelf)).forEach((o, index) => {
-                data.push(<option value={[o.bookID, o.shelf]}
-                                  key={index}
-                                  className={"text-capitalize"}>{o.shelf.replace( rex, '$1$4 $2$3$5' )}</option>)
-            }
-        )
-        return data;
-    }
+    let bgImg = book?.imageLinks?.thumbnail ?? 'imgs/no-img.jpg';
 
     return (
        <li>
@@ -35,19 +20,20 @@ export const BookItem = (props) => {
                <div className="book-top">
                    <div
                        className="book-cover"
-                       style={{backgroundImage: 'url("'+ props.book.imageLinks.thumbnail +'")'}}
+                       style={{backgroundImage: 'url("'+ bgImg +'")'}}
                    ></div>
                    <div className="book-shelf-changer">
-                       <select onChange={moveShelf} defaultValue={"none"}>
-                           <option value="none" disabled>
-                               Move to...
-                           </option>
-                           { renderOptional(props.book) }
+                       <select onChange={e => moveShelf(e.target.value)} defaultValue={(book.id+','+book.shelf) ?? "none"}>
+                           <option value="none" disabled> Move to...</option>
+                           <option value={[book.id, shelfEnum.READING]}> Currently Reading </option>
+                           <option value={[book.id, shelfEnum.WANT_TO_READ]}> Want To Read </option>
+                           <option value={[book.id, shelfEnum.READ]}> Read </option>
+                           <option value={[book.id, shelfEnum.NONE]}> None </option>
                        </select>
                    </div>
                </div>
-               <div className="book-title">{ props.book.title }</div>
-               <div className="book-authors">{ props.book.authors.toString() }</div>
+               <div className="book-title">{ book.title }</div>
+               <div className="book-authors">{ book?.authors?.toString() ?? '' }</div>
            </div>
        </li>
     )
